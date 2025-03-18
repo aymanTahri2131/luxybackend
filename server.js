0afb33d0880dfe40758bd618e2253ae074998a60
aymanTahri2131 <th.ayman.96@gmail.com>
2025-03-18 04:58:19 +0000

    3rd commit for deployment

--- server.js
+++ server.js
@@ -14,28 +14,15 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
+app.use(cors({
+  origin: "https://luxy-marbre.netlify.app", // ✅ Remplace par l'URL Netlify
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization"
+}));
+
 //middlewares
 
 app.use(express.json());
-
-const allowedOrigins = ['http://localhost:3000', 'https://luxy-marbre.netlify.app/'];
-
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, token');
-
-  // Intercepter les requêtes OPTIONS pour les requêtes préliminaires
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
 
 //api endpoint
 
